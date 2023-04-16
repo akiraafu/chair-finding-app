56a6c1170a67897b476211462396536ba3f91c51
Junrong Fu <junrongfu6@gmail.com>
2023-04-16 19:30:53 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <a href="" className="btn btn-primary">
+        click me
+      </a>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbeov2-HXoVVNOI_V5ojZa9FFTIj1XB8c",
+  authDomain: "chair-finding-app.firebaseapp.com",
+  projectId: "chair-finding-app",
+  storageBucket: "chair-finding-app.appspot.com",
+  messagingSenderId: "409624826295",
+  appId: "1:409624826295:web:92a39f96a4f8d3b759d832",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
