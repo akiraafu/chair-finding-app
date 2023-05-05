fe2c74bd5dad21282645d9273edf94ed32fba9a1
Junrong Fu <junrongfu6@gmail.com>
2023-05-05 23:37:26 +0800

    make user page working, list all the items post by the same user

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -6,34 +6,32 @@ import { useDocument } from "../../hooks/useDocument";
 
 import Avatar from "../../components/Avatar";
 import { useEffect } from "react";
+import ItemList from "../../components/ItemList";
 
 const User = () => {
   const { id } = useParams();
-  const { document, error } = useDocument("users", id);
+  const { document } = useDocument("users", id);
+  const { documents, error } = useCollection("items");
 
-  // const { user } = useAuthContext();
-  // const { documents, error } = useCollection("items");
-
-  // useEffect(() => {
-  //   const userItems = [];
-  //   const authorId = documents && documents.map((d) => d.createdBy.id);
-  //   const userId = user && user.uid;
-
-  //   if (authorId === userId) {
-  //   }
-
-  //   console.log(authorId, userId);
-  // }, [documents]);
+  let userItems = null;
+  if (documents) {
+    userItems = documents.filter((d) => d.createdBy.id === id);
+  }
 
   return (
     <div className="w-full h-[calc(100vh-140px)]">
       {error && <p className="error">{error}</p>}
 
       {document && (
-        <div className="w-full my-3 user flex flex-col justify-center items-center gap-1">
-          <Avatar src={document.photoURL} />
-          <p className="font-bold text-sm">{document.displayName}</p>
-        </div>
+        <>
+          <div className="w-full my-3 user flex flex-col justify-center items-center gap-1">
+            <Avatar src={document.photoURL} />
+            <p className="font-bold text-sm">{document.displayName}</p>
+          </div>
+          <div className="h-full overflow-y-scroll scrollbar-hide flex flex-col items-center">
+            {userItems && <ItemList items={userItems} />}
+          </div>
+        </>
       )}
     </div>
   );
