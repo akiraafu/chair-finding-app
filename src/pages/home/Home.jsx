1364a5daa560b46c3de4a8286d7a39cda05fe4b4
Akira <junrongfu6@gmail.com>
2023-07-26 17:33:52 +0800

    changed background

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Background from "../../assets/bg.jpg";
+import Video from '../../assets/video-bg.mp4'
 
 const Home = () => {
   return (
     <div
-      className="w-full h-[calc(100vh-140px)] bg-no-repeat bg-hero bg-cover bg-center bg-fixed flex flex-col justify-center items-center"
-      style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${Background})`,
-      }}
+      className="w-screen h-[calc(100vh-140px)] relative bg-black bg-opacity-50 flex flex-col justify-center items-center overflow-hidden "
+     
     >
+      <img src={Background} className="absolute h-full object-cover left-0 -z-10 lg:invisible" />
+      <video autoPlay loop muted class="absolute w-full -z-20 hidden lg:block  " >
+                <source src={Video} type="video/mp4" />
+      </video>
       <h1 className="text-white text-2xl md:text-5xl font-bold mb-5">
-        Chiar Issue
+        Chair Issue
       </h1>
       <p className="text-white max-w-md md:max-w-xl px-3 mb-2">
         Welcome to Chair Issue!
       </p>
       <p className="text-white text-center px-3">
-        Jump right in and explor free chairs shared by our friendly users.
+        Jump right in and explore free chairs shared by our friendly users.
       </p>
       <p className="text-white mb-5 text-center px-3">
         Feel free to share some if you have any or find any chairs for others to
